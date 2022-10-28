bff78ccc21ede551ba5409aaa55cebce1cdef9d7
Bauke <me@bauke.xyz>
2022-10-28 14:06:03 +0200

    Add a with-hooks test.

--- tests/parsing.rs
+++ tests/parsing.rs
@@ -5,6 +5,7 @@ use {color_eyre::Result, hooked::Config};
 use {insta::assert_debug_snapshot, test_case::test_case};
 
 #[test_case("defaults" ; "defaults")]
+#[test_case("with-hooks" ; "with-hooks")]
 fn test_config_parsing(file_name: &str) -> Result<()> {
   let file = PathBuf::from(format!("tests/parsing/{file_name}.toml"));
   let config = Config::from_toml_file(file)?;
