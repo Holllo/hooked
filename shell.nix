d12895184b74f293e3f5b1544f9092e9d18c89cd
Bauke <me@bauke.xyz>
2024-01-19 15:33:01 +0100

    Install tools in a local output directory.

--- shell.nix
+++ shell.nix
@@ -15,9 +15,14 @@ mkShell rec {
   ];
 
   shellHook = ''
+    # Add the outputs directory to PATH where local tools will be installed.
+    PATH="$PATH:$out/bin"
+
     # If Hooked isn't installed, use cargo to install the local version of it.
     if ! [[ -x "$(command -v hooked)" ]]; then
-      cargo install --path hooked-cli
+      cargo install --path hooked-cli --root $out
     fi
+
+    hooked install
   '';
 }
